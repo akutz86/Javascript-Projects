43adfd2cb25ee396074eeb15ef852d0ddefd443f
Amanda Kutz <amanda.m.kutz@gmail.com>
2021-11-30 23:44:32 -0700

    Complete Javascript Assignment

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,10 +1,17 @@
-var family = "The Smiths", mom = "Helga", dad = "Bart", daughter = "Sofia", son = "Odin";
+window.alert("Mary had a little lamb") //shows popup window with text and button
 
-var family = family.fontcolor("blue");
-var mom = mom.fontcolor("purple");
-var dad = dad.fontcolor("green");
-var daughter = daughter.fontcolor("red");
-var son = son.fontcolor("orange")
-document.write(family);
+var A = "Mary had a little lamb" //created variable A
+var B = "whose fleece was white as snow" //created variable B
 
-document.write(3*9);
+document.write(A + B) //concatenating the string variables A and B
+
+var family = "The Smiths", mom = "Helga", dad = "Bart", daughter = "Sofia", son = "Odin"; //defining the variables, family, mom, dad, daughter and son
+
+var family = family.fontcolor("blue"); //changed font color for family to blue
+var mom = mom.fontcolor("purple"); //changed font color for mom to purple
+var dad = dad.fontcolor("green"); //changed font color for dad to green
+var daughter = daughter.fontcolor("red"); //changed font color for daughter to red
+var son = son.fontcolor("orange"); //changed font color for son to orange
+document.write(family); //prints the family variable
+
+document.write(3*9); //prints the expression 3 multipled by 9
