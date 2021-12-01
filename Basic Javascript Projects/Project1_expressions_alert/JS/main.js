269d91fb0296bddbbfa3b78cf07d5278fbf91e8b
Amanda Kutz <amanda.m.kutz@gmail.com>
2021-11-30 23:34:10 -0700

    First Javascript project

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var family = "The Smiths", mom = "Helga", dad = "Bart", daughter = "Sofia", son = "Odin";
+
+var family = family.fontcolor("blue");
+var mom = mom.fontcolor("purple");
+var dad = dad.fontcolor("green");
+var daughter = daughter.fontcolor("red");
+var son = son.fontcolor("orange")
+document.write(family);
+
+document.write(3*9);
