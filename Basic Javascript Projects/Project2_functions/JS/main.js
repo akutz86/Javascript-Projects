d9967107f858164937091c83714130ce699f5561
Amanda Kutz <amanda.m.kutz@gmail.com>
2021-12-01 20:23:08 -0700

    JS functions project

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -1,7 +1,13 @@
-function myFunction() {
-    var str = "Look at me now";
-    var result = str.fontsize(7);
-    document.getElementById("largetext").innerHTML = result;
+function my_first_function() { //defining my_first_function
+    var str = "Look at me now"; //establishing string variable
+    var result = str.fontsize(7); //establishing font size string variable should be printed at
+    document.getElementById("largetext").innerHTML = result; //printing string variable at font size set in prior step
+} 
+
+function myFunction() { //defining myFunction
+    var sentence = "I am leanring"; //establishing variable sentence
+    sentence += " a lot from this book"; //adding another part to sentence above
+    document.getElementById("Concatenate").innerHTML = sentence; //printing concatenated sentence variables
 }
 
 
